b2a2c97cd80299ebd267804d924e94d2e61ee84f
jilongcui <jilongcui@163.com>
2022-07-05 19:48:17 +0800

    Add bitlove.com website link.

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="app-container home">
     <h3>
-      <el-link type="primary" href="https://87789771.github.io/#/"
-        >查看开发文档请点击</el-link
-      >
+      <el-link type="primary" href="https://www.bitlove.com">爱芯官网</el-link>
     </h3>
     <h3>
-      <el-link
-        type="primary"
-        href="https://github.com/87789771/meimei-nestjs-admin"
-        >查看码源请点击</el-link
-      >
+      <el-link type="primary" href="admin.bitlove.com">爱芯后台管理</el-link>
     </h3>
   </div>
 </template>
