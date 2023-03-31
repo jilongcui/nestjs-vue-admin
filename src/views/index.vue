eda5cf1a4a7b208dfe5033d0f7fc369a72ccaa6f
jilongcui <jilongcui@163.com>
2023-03-31 18:47:47 +0800

    Remove unuse info.

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="app-container home">
-    <h3>
-      <el-link type="primary" href="https://www.startland.top">官网</el-link>
-    </h3>
-    <h3>
-      <el-link type="primary" href="admin.startland.top">后台</el-link>
+    <!-- <h3>
+      <el-link type="primary" href="/">官网</el-link>
     </h3>
   </div>
 </template>
